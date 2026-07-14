d181601d0fd1818ef7f8b569cfba6cfae2b3e740
Uttam Kothari <uttamkothari6934@gmail.com>
2026-07-14 17:08:39 +0530

    Add files via upload

---
+++ Lab-2/A5.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class A5 {
+    
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter m: ");
+        int b=sc.nextInt();
+
+        System.out.println("feet="+(b*3.8));
+    }
+}
