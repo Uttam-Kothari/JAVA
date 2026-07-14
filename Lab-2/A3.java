4492e929b291261998f91e53625b06be66367e7f
Uttam Kothari <uttamkothari6934@gmail.com>
2026-07-14 17:07:05 +0530

    Add files via upload

---
+++ Lab-2/A3.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class A3{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter a: ");
+        int a=sc.nextInt();
+        System.out.println("a="+a);
+    }
+}
