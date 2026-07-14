e47e7f2ab47db82d2c3147e6c3d72c79951f0468
Uttam Kothari <uttamkothari6934@gmail.com>
2026-07-14 17:05:52 +0530

    Add files via upload

---
+++ Lab-2/A2.java
@@ -0,0 +1,13 @@
+public class A2{
+    public static void main(String[] args){
+        int a=Integer.parseInt(args[0]);
+        System.out.println("a="+a);
+        float b=Float.parseFloat(args[1]);
+        System.out.println("b="+b);
+        double c=Double.parseDouble(args[2]);
+        System.out.println("c="+c);
+        char d=args[3].charAt(0);
+        System.out.println("d="+d);
+    }
+
+}
