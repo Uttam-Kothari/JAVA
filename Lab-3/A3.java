56d3076cdcbaa7a0976e08b81dacc71f84d85cc3
Uttam Kothari <uttamkothari6934@gmail.com>
2026-07-18 17:47:19 +0530

    Add files via upload

---
+++ Lab-3/A3.java
@@ -0,0 +1,14 @@
+package lab3;
+import java.util.Scanner;
+
+public class A3 {
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("enter the value of hight: ");
+            Float h=sc.nextFloat();
+            System.out.println("enter the value of width: ");
+            Float w=sc.nextFloat();
+            System.out.println("Area of rectangle="+(h*w));
+
+        }    
+}
